ba17dc74f60e29da9c94aa3055593b810b76255a
perXautomatik <christoffer.broback@gmail.com>
2021-11-23 21:24:16 +0100

    dud

---
+++ Untitled1.ps1
@@ -0,0 +1,17 @@
+﻿take all extensions
+take all date created
+
+do until each file is moved into folder x.
+
+    for i=0 to each
+         do case ( on each date created falling downwards, allowing failed renamings to be included)
+            rename files each file with relevant extension or date created.
+                don't force, fail on atempt. 
+
+Move each filed renamed to folder x
+    force overwrite
+
+git folder x
+ add message 
+    for example 
+        fileSize of files in folder concacnated as string
